020a80f19680efb60c505c9a87ad2c58b5f651a2
kubo11 <72470248+kubo11@users.noreply.github.com>
2026-04-21 02:20:43 +0200

    Use btrfs

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 KEYMAP="pl"
 TIMEZONE="Europe/Warsaw"
+LANG="en_US.UTF-8"
 DISK="$1"
-EFI="${DISK}1"
-SWAP="${DISK}2"
-ROOT="${DISK}3"
+EFI_PART="${DISK}1"
+SWAP_PART="${DISK}2"
+ROOT_PART="${DISK}3"
 HOSTNAME="kubopc"
 
 if [ -z "$DISK" ] ; then
@@ -21,31 +22,49 @@ timedatectl set-timezone "$TIMEZONE"
 timedatectl set-ntp true
 
 echo "Setting up system drive..."
-wipefs -a "$DISK"
-sfdisk "$DISK" <<EOF
+# wipefs -af "$DISK"
+
+sfdisk -w always -W always "$DISK" <<EOF
 label: gpt
 
-${DISK}p1 : size=1024M, type=U
-
-${DISK}p2 : size=4096M, type=S
-
-${DISK}p3 : type=L
+$EFI_PART : size=1024M, type=U
+$SWAP_PART: size=8192M, type=S
+$LVM_PART : type=L
 EOF
 
 partprobe "$DISK"
 sleep 2
 
-mkfs.fat -F32 "$EFI"
-mkswap "$SWAP"
-mkfs.ext4 "$ROOT"
+mkfs.fat -F32 "$EFI_PART"
+mkswap "$SWAP_PART"
+mkfs.btrfs "$ROOT_PART"
+
+echo "Creating btrfs subvolumes..."
+mount "$ROOT_PART" /mnt
+
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@pkg
+btrfs subvolume create /mnt/@.snapshots
+
+umount /mnt
 
 echo "Mounting rootfs..."
-mount "$ROOT" /mnt
-mount --mkdir "$EFI" /mnt/boot
-swapon "$SWAP"
+mount -o noatime,compress=zstd,subvol=@ "$ROOT_PART" /mnt
+
+mkdir -p /mnt/{var/log,var/cache/pacman/pkg,boot/efi}
+
+mount --mkdir -o noatime,compress=zstd,subvol=@home "$ROOT_PART" /mnt/home
+mount --mkdir -o noatime,compress=zstd,subvol=@.snapshots "$ROOT_PART" /mnt/.snapshots
+mount -o noatime,compress=zstd,subvol=@log "$ROOT_PART" /mnt/var/log
+mount -o noatime,compress=zstd,subvol=@pkg "$ROOT_PART" /mnt/var/cache/pacman/pkg
+
+mount "$EFI_PART" /mnt/boot/efi
+swapon "$SWAP_PART"
 
 echo "Installing essential software..."
-pacstrap -K /mnt base linux linux-firmware grub efibootmgr
+pacstrap -K /mnt base linux linux-firmware grub efibootmgr btrfs-progs git python3
 
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -54,10 +73,10 @@ echo "Writiing install-chrooted.sh to /mnt/root..."
 echo "#!/bin/sh
 
 echo \"Setting time & locale...\"
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 hwclock --systohc
 locale-gen
-echo \"LANG=en_US.UTF-8\" >> /etc/locale.conf
+echo \"LANG=${LANG}\" >> /etc/locale.conf
 echo \"KEYMAP=${KEYMAP}\" >> /etc/vconsole.conf
 
 echo \"Setting hostname...\"
@@ -67,7 +86,7 @@ echo \"Setting password...\"
 passwd
 
 echo \"Configuring boot loader...\"
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 exit" >/mnt/root/install-chrooted.sh
